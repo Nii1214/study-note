1251c0250c2f5f6eb7bb8eba80cc4025f0bf78a6
Nii1214 <nii.tomoyuki1214@gmail.com>
2025-04-13 13:58:24 +0900

    ユーザー登録処理を実装

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
+console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('Supabase API Key:', import.meta.env.VITE_SUPABASE_API_KEY);
 
+export const supabase = createClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_API_KEY
-    
 );
