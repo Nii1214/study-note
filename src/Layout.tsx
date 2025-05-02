0a5c294afbb01cabbac9788591a88398a547e092
Nii1214 <nii.tomoyuki1214@gmail.com>
2025-05-02 15:49:07 +0900

    検索モーダルの開閉処理を実装

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -10,6 +10,7 @@ const Layout = () => {
     const { currentUser } = useCurrentUserStore();
     const noteStore = useNoteStore();
     const [isLoading,setIsLoading] = useState(false);
+    const [isShowModal,setIsShowModal] = useState(false);
 
     useEffect(()=> {
       fetchNotes();
@@ -29,15 +30,17 @@ const Layout = () => {
 
   return (
     <div className="h-full flex dark:bg-[#1e1e1e] dark:text-white">
-      {!isLoading && <SideBar onSearchButtonClicked={() => {}} />}
+      {!isLoading && (
+        <SideBar onSearchButtonClicked={() => setIsShowModal(true)} 
+      />)}
       <main className="flex-1 h-full overflow-y-auto">
         <Outlet />
         <SearchModal
-          isOpen={false}
+          isOpen={isShowModal}
           notes={[]}
           onItemSelect={() => {}}
           onKeywordChanged={() => {}}
-          onClose={() => {}}
+          onClose={() => setIsShowModal(false)}
         />
       </main>
     </div>
